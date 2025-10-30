a2f0eb46a8b08a47377f5c5ec59d8bf340315fe2
Renan Kenji Oshiro <103371753+RenanOshiro@users.noreply.github.com>
2025-10-29 23:33:13 -0300

    Finish ex 13_palindromes all tests passed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let array = string.split('');
 
+    let semPontuacao = array.filter(char => {
+        return !/\p{P}|\s/u.test(char);
+    })
+
+    let reverse = [...semPontuacao].reverse();
+
+    for (let i = 0; i < semPontuacao.length; i++) {
+        if (semPontuacao[i].toLowerCase() !== reverse[i].toLowerCase()) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
