cc7c074df4d9e7cffcb83d99312e2d0e3985ccf8
Renan Kenji Oshiro <103371753+RenanOshiro@users.noreply.github.com>
2025-10-31 00:39:27 -0300

    Finish ex 14_fibonacci all tests passed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    let fibo = [1, 1];
 
+    if (position == 0) {return 0;}
+    else if (position == 1) {return 1;}
+    else if (position < 0) {return "OOPS";}
+    for (let i = 1; i < position-1; i++) {
+        fibo.push(fibo[i] + fibo[i-1]);
+    }
+    return fibo[fibo.length-1];
 };
 
 // Do not edit below this line
